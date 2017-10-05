c75e3c18c025f4c311027d53fb509c1a4d6e1963
fdelgado <fdelgado@itba.edu.ar>
2017-10-05 10:56:55 -0300

    Parsefile v1

--- AVLTreeInBlockChain.java
+++ AVLTreeInBlockChain.java
@@ -104,7 +104,53 @@ public class AVLTreeInBlockChain {
 
 	/**Parsea el archivo y agrega a la blockchain el bloque descripto en el archivo.
 	 * */
-	private static void parseFile(BufferedReader buff)throws InvalidFileFormatException{
+	private static void parseFile(BufferedReader buff) throws InvalidFileFormatException, IOException {
+
+		String blockIDStr = buff.readLine();
+		String nonceStr = buff.readLine();
+		String dataStr = buff.readLine();
+		String prevHashStr = buff.readLine();
+
+		String [] IDarr = blockIDStr.split(": ");
+		if(IDarr.length > 2)
+			throw new InvalidFileFormatException("formato del BlockID no compatible");
+		int blockID = Integer.parseInt(IDarr[1]);
+
+		String [] nonceArr =  nonceStr.split(": ");
+		if(IDarr.length > 2)
+			throw new InvalidFileFormatException("formato del nonce no compatible");
+		int nonce = Integer.parseInt(nonceArr[1]);
+
+		// hay que estandarisar los nombres de las operaciones para el archivo
+		String [] DataArr =  dataStr.split(": ");
+		if(IDarr.length > 4)
+			throw new InvalidFileFormatException("formato de los datos no compatible");
+		String operation = DataArr[1];
+
+		//el arbol debe estar como una list en formato BFS en el archivo
+		//me parece que BFS es la mejor manera asi no hace rotaciones cuando se crea el nuevo arbol
+		String[] nodes = DataArr[2].split(",");
+		AVLTree tree = new AVLTree();
+		for(int i = 0; i< nodes.length; i++){
+			tree.insert(Integer.parseInt(nodes[i]));
+		}
+
+		String[] modifiedArr = DataArr[3].split(",");
+		if(modifiedArr.length == 0)
+			throw  new InvalidFileFormatException("no hay nodos modificados en el array de nodos modificados");
+		int[] modified = new int[modifiedArr.length];
+		for(int i = 0;i<modifiedArr.length; i++){
+			modified[i] = Integer.parseInt(modifiedArr[i]);
+		}
+
+		String [] prevHashArr =  prevHashStr.split(": ");
+		if(IDarr.length > 2)
+			throw new InvalidFileFormatException("formato del prevHash no compatible");
+		int prevHash = Integer.parseInt(prevHashArr[1]);
+
+		//hay que crear un bloque con blockID, nonce, operacion, tree, modificados , prevhash y despues
+		//agregarlo a la blockchain
+
 
 	}
 }
